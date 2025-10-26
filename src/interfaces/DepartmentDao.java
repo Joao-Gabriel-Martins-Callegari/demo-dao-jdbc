9c23c7af3e87b796f59453a5719ecef0da25483c
João Gabriel <joaobielcalle@gmail.com>
2025-10-26 17:14:43 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/interfaces/DepartmentDao.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+import java.util.List;
+
+import entities.Department;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
+}
