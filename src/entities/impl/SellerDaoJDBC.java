2f1fb3ef27f96843af5ee5809e579284fceb0960
João Gabriel <joaobielcalle@gmail.com>
2025-11-05 18:28:31 -0300

    update implementation

--- src/entities/impl/SellerDaoJDBC.java
+++ src/entities/impl/SellerDaoJDBC.java
@@ -27,12 +27,12 @@ public class SellerDaoJDBC implements SellerDao {
     @Override
     public void insert(Seller obj) {
         PreparedStatement st = null;
-        try{
+        try {
             st = conn.prepareStatement("INSERT INTO seller \r\n" + //
-                                "(Name, Email, BirthDate, BaseSalary, DepartmentId)  \r\n" + //
-                                "VALUES  \r\n" + //
-                                "(?, ?, ?, ?, ?) ", 
-                                Statement.RETURN_GENERATED_KEYS);
+                    "(Name, Email, BirthDate, BaseSalary, DepartmentId)  \r\n" + //
+                    "VALUES  \r\n" + //
+                    "(?, ?, ?, ?, ?) ",
+                    Statement.RETURN_GENERATED_KEYS);
 
             st.setString(1, obj.getName());
             st.setString(2, obj.getEmail());
@@ -42,26 +42,46 @@ public class SellerDaoJDBC implements SellerDao {
 
             int rowsAffected = st.executeUpdate();
 
-            if(rowsAffected > 0){
+            if (rowsAffected > 0) {
                 ResultSet rs = st.getGeneratedKeys();
-                if(rs.next()){
+                if (rs.next()) {
                     int id = rs.getInt(1);
                     obj.setId(id);
                 }
                 DB.closeResultSet(rs);
-            }else{
+            } else {
                 throw new DbException("Erro inesperado, nenhuma linha foi afetada!!!");
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             throw new DbException(e.getMessage());
-        }finally{
+        } finally {
             DB.closeStatement(st);
         }
     }
 
     @Override
     public void update(Seller obj) {
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement("UPDATE seller  \r\n" + //
+                    "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?  \r\n" + //
+                    "WHERE Id = ?",
+                    Statement.RETURN_GENERATED_KEYS);
+
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
@@ -130,12 +150,12 @@ public class SellerDaoJDBC implements SellerDao {
             while (rs.next()) {
 
                 Department dep = map.get(rs.getInt("DepartmentId"));
-                
-                if(dep == null){
+
+                if (dep == null) {
                     dep = instantiateDepartment(rs);
                     map.put(rs.getInt("DepartmentId"), dep);
                 }
-                
+
                 Seller obj = instantiateSeller(rs, dep);
                 list.add(obj);
             }
@@ -149,7 +169,7 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     @Override
-    public List<Seller> findAll(){
+    public List<Seller> findAll() {
         PreparedStatement st = null;
         ResultSet rs = null;
         try {
@@ -166,12 +186,12 @@ public class SellerDaoJDBC implements SellerDao {
             while (rs.next()) {
 
                 Department dep = map.get(rs.getInt("DepartmentId"));
-                
-                if(dep == null){
+
+                if (dep == null) {
                     dep = instantiateDepartment(rs);
                     map.put(rs.getInt("DepartmentId"), dep);
                 }
-                
+
                 Seller obj = instantiateSeller(rs, dep);
                 list.add(obj);
             }
