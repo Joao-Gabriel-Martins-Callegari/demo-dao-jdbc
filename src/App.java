a89804ad809fbdef4ca3432691ee5de5be7be50d
João Gabriel <joaobielcalle@gmail.com>
2025-11-05 18:19:14 -0300

    insert implementation

--- src/App.java
+++ src/App.java
@@ -29,6 +29,10 @@ public class App {
             System.out.println(obj);
         }
 
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller =  new Seller(0, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted!! New id = " + newSeller.getId());
 
     }
 }
